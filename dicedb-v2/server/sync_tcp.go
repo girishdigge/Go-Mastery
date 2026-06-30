d3406d8e6a406563413967e9b5f4477c3c801262
girishdigge <girishdigge@gmail.com>
2026-06-30 11:04:30 +0000

    PING implementation

--- dicedb-v2/server/sync_tcp.go
+++ dicedb-v2/server/sync_tcp.go
@@ -1,28 +1,38 @@
 package server
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"strconv"
+	"strings"
 
 	"github.com/dicedb/dice/config"
+	"github.com/dicedb/dice/core"
 )
 
-func readCommand(c net.Conn) (string, error) {
+func readCommand(c net.Conn) (*core.RedisCmd, error) {
 	var buf []byte = make([]byte, 512)
 	n, err := c.Read(buf[:])
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(buf[:n]), nil
+	tokens, err := core.DecodeArrayString(buf[:n])
+	return &core.RedisCmd{
+		Cmd:  strings.ToUpper(tokens[0]),
+		Args: tokens[1:],
+	}, nil
 }
 
-func respond(cmd string, c net.Conn) error {
-	if _, err := c.Write([]byte(cmd)); err != nil {
-		return err
+func respondError(err error, c net.Conn) {
+	c.Write([]byte(fmt.Sprintf("-%s\r\n", err)))
+}
+func respond(cmd *core.RedisCmd, c net.Conn) {
+	err := core.EvalAndRespond(cmd, c)
+	if err != nil {
+		respondError(err, c)
 	}
-	return nil
 }
 
 func RunSyncTCPServer() {
@@ -54,10 +64,8 @@ func RunSyncTCPServer() {
 				}
 				log.Println("err", err)
 			}
-			log.Println("command", cmd)
-			if err = respond(cmd, c); err != nil {
-				log.Print("err write:", err)
-			}
+
+			respond(cmd, c)
 		}
 
 	}
