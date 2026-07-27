7655797c84586ca77af0e64caa87cda8bb47f4ff
girishdigge <girishdigge@gmail.com>
2026-07-27 05:31:23 +0000

    CSRF Token added

--- hello-world/cmd/web/routes.go
+++ hello-world/cmd/web/routes.go
@@ -12,7 +12,7 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
-	mux.Use(WriteToMiddleware)
+	mux.Use(NoSurf)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
--- hello-world/go.mod
+++ hello-world/go.mod
@@ -5,4 +5,5 @@ go 1.26.1
 require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.3.1
+	github.com/justinas/nosurf v1.2.0
 )
